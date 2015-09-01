b51f8bbd726dd3d689de4fd43da4e6c4cb6d9ade
OneMoreNerd <OneMoreNerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-01 19:59:22 +0000

    Tagged version 0.2.0
    git-svn-id: https://plugins.svn.wordpress.org/cb-parallax/tags/0.2.0@1235704 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-cb-parallax-deactivator.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * @link              https://github.com/demispatti/cb-parallax/
+ * @since             0.1.0
+ * @package           cb_parallax
+ * @subpackage        cb_parallax/includes
+ * Author:            Demis Patti <demis@demispatti.ch>
+ * Author URI:        http://demispatti.ch
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+class cb_parallax_deactivator {
+
+	/**
+	 * The variable that holds the name of the capability which is necessary
+	 * to interact with this plugin.
+	 *
+	 * @since    0.1.0
+	 * @access   public
+	 * @var      string $capability
+	 */
+	public static $capability = 'cb_parallax_edit';
+
+	/**
+	 * Fired during deactivation of the plugin.
+	 * Removes the capability to edit custom backgrounds from the administrator role.
+	 *
+	 * @hooked_action
+	 *
+	 * @since    0.1.0
+	 * @access   static
+	 * @return   void
+	 */
+	public static function deactivate() {
+
+		// Gets the administrator role.
+		$role = get_role( 'administrator' );
+
+		// If the acting user has admin rights, the capability gets removed.
+		if ( ! empty( $role ) ) {
+			$role->remove_cap( self::$capability );
+		}
+	}
+}
